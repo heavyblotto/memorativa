2b0da7a3a6bd8a48acd544c7e5768fd73906316f
Plog Pasternak <39931321+heavyblotto@users.noreply.github.com>
2025-01-31 08:09:10 -0500

    updates

--- memorativa/src/app/page.tsx
+++ memorativa/src/app/page.tsx
@@ -7,18 +7,17 @@ export default function Home() {
         <Image
           className="dark:invert"
           src="/next.svg"
-          alt="Next.js logo"
+          alt="Memorativa logo"
           width={180}
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Welcome to Memorativa{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
-            .
           </li>
           <li>Save and see your changes instantly.</li>
         </ol>
